a9dd601f94279e8f459e8d7e9270db38cd91ef17
zainish24 <164177511+zainish24@users.noreply.github.com>
2026-03-13 12:33:16 -0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/developers/light.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/developers/dark.png',
         media: '(prefers-color-scheme: dark)',
       },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
     ],
-    apple: '/apple-icon.png',
+    apple: '/developers/light.png',
   },
 }
 
